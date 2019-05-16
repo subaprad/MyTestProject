23592a0ca233bfeb58e307699733d9231fdd518f
Subas Pradhan -X (subaprad - SCARLET WIRELESS INDIA PRIVATE LIMITED at Cisco) <subaprad@cisco.com>
2019-05-16 14:58:14 +0530

    .sh file change

--- pre_run.sh
+++ pre_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #pytest --cov=.
-pytest --cov=. --cov-report html:coverage
+#pytest --cov=. --cov-report html:coverage
+pytest --junitxml=pytest/test_output.xml --cov=. --cov-report html:coverage
